8d32344e7e09176ae60b8aeef885bd16cc4c7f80
AndresBaez96 <afbaezm@unal.edu.co>
2022-03-15 22:22:36 -0500

    Implemented logic, bug in function replaceChar with duplicated letters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def read():
     words = dict()
@@ -12,10 +13,35 @@ def randomNumber(dict : dict):
     word = dict.get(n)
     return word
 
+def mysteryWord(word : str):
+    dashWord = ['-' for i in range(len(word))]
+    newWord = ''.join(dashWord)
+    return newWord, dashWord
+
+def replaceChar(char : str, word : str, dashWord : list):
+    for i in word:
+        if i == char:
+            index = word.index(char)
+            dashWord[index] = char
+    dashWord = ''.join(dashWord)
+    return dashWord
+
 def run():
     words = read()
     word = randomNumber(words)
-    print(word)
+    word2Find, list_word = mysteryWord(word)
+    
+    while True:
+        os.system('clear')
+        print(word, word2Find)
+        char = input('Enter a letter: ')
+        char = char.lower()
+        word2Find = replaceChar(char, word, list_word)
 
+        
+        
+    
+    
+    
 if __name__ == '__main__':
     run()
